60e9b40031c4b3179797a918dfcf55fd966f74bf
Sreekar <Shree@email.com>
2018-07-12 13:02:42 -0400

    Committing Greeting.java and HelloWorld.java

---
+++ src/com/veridic/practice/Greeting.java
@@ -0,0 +1,7 @@
+package com.veridic.practice
+
+public class Greeting {
+	public String greet(String name) {
+		return "Hello" + name + "!!";
+}
+}
---
+++ src/com/veridic/practice/HelloWorld.java
@@ -0,0 +1,6 @@
+package com.veridic.java
+	public static main void (String[] args) {
+		Greeting obj = new Greeting();
+		System.out.println (obj.greet("Jenkins"));
+}
+}
